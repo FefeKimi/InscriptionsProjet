a3d7f87123470eb4303f80ad27875df8b623d0c0
Fabrice Nguy <Fabrice Nguy@192.168.1.24>
2017-03-04 18:25:18 +0100

    Pizza

--- ProjetInscription/src/Connect.java
+++ ProjetInscription/src/Connect.java
@@ -5,85 +5,100 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.Month;
 
 import com.mysql.jdbc.CallableStatement;
 
 //CTRL + SHIFT + O pour générer les imports
 public class Connect {
     public static void main(String[]args){
-    	requete("DELETE from EQUIPE WHERE NumCandidat=1");
-    	//readBDD("SELECT * FROM CANDIDAT");
-    
+     Connect c = new Connect();
+     /*LocalDate dateCloture = LocalDate.of(2017,Month.APRIL,10);
+     c.addComp("badminton", dateCloture,false);*/
+     c.addPersonne("Jules","Cesar","jc@gmail.com");
     }
-	public static Statement requete(String requete) {
-	
-	Statement st =null;
-	try {
+    
+ public static void requete(String requete) {
+ 
+ Statement st =null;
+ try {
       Class.forName("com.mysql.jdbc.Driver");
       System.out.println("Driver O.K.");
 
       String url = "jdbc:mysql://localhost/inscription2017?useSSL=false";
-	  String login= "root";
-	  String passwd = "";
+   String login= "root";
+   String passwd = "";
 
       Connection conn = DriverManager.getConnection(url, login, passwd);
       System.out.println("Requête executée !"); 
       st=conn.createStatement();
-		
+  
       st.executeUpdate(requete);
          
     } catch (Exception e) {
       e.printStackTrace();
     }
-	return st;
-	}
-	public static ResultSet readBDD(String requete,String id, String nom) {
 
-		// Information d'accès à la base de données
+ }
+ public static ResultSet readBDD(String requete,String id, String nom) {
 
-		Connection cn =null;
-		Statement st =null;
-		ResultSet rs = null;
-		String url = "jdbc:mysql://localhost/inscription2017?useSSL=false";
-		String login= "root";
-		String passwd = "";
-		
-		try {
+  // Information d'accès à la base de données
 
-			// Etape 1 : Chargement du driver
-			Class.forName("com.mysql.jdbc.Driver");
+  Connection cn =null;
+  Statement st =null;
+  ResultSet rs = null;
+  String url = "jdbc:mysql://localhost/inscription2017?useSSL=false";
+  String login= "root";
+  String passwd = "";
+  
+  try {
 
-			// Etape 2 : récupération de la connexion
-			cn = DriverManager.getConnection(url, login, passwd);
+   // Etape 1 : Chargement du driver
+   Class.forName("com.mysql.jdbc.Driver");
 
-			// Etape 3 : Création d'un statement
-			st = cn.createStatement();
+   // Etape 2 : récupération de la connexion
+   cn = DriverManager.getConnection(url, login, passwd);
 
-			String sql = requete;
+   // Etape 3 : Création d'un statement
+   st = cn.createStatement();
 
-			// Etape 4 : exécution requête
-			rs = st.executeQuery(sql);
-			System.out.println();
-			while(rs.next()){
-				System.out.print("| ");
-	    		System.out.print(rs.getString(id));
-	    		System.out.print(" | ");
-	    		System.out.println(rs.getString(nom));
-	    	}	System.out.println();
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-		
-				cn.close();
-				st.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return rs;
-	}
+   String sql = requete;
+
+   // Etape 4 : exécution requête
+   rs = st.executeQuery(sql);
+   System.out.println();
+   while(rs.next()){
+    System.out.print("| ");
+       System.out.print(rs.getString(id));
+       System.out.print(" | ");
+       System.out.println(rs.getString(nom));
+      } System.out.println();
+   
+  } catch (SQLException e) {
+   e.printStackTrace();
+  } catch (ClassNotFoundException e) {
+   e.printStackTrace();
+  } finally {
+   try {
+  
+    cn.close();
+    st.close();
+   } catch (SQLException e) {
+    e.printStackTrace();
+   }
+  }
+  return rs;
+ }
+ 
+ /*inscriptions*/
+ public void addComp(String nom, LocalDate dateCloture, boolean enEquipe){
+  Connect.requete("call ADD_COMP('"+nom+"','"+dateCloture+"',"+enEquipe+")");
+ }
+ 
+ public void addPersonne(String nom,String prenom, String mail){
+  Connect.requete("call ADD_PERS('"+nom+"','"+mail+"',"+prenom+")");
+ }
+ 
+ 
 }
