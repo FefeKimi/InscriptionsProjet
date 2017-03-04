6853cc323e5bc133b1f9f8a1131972199dc9af00
FefeKimi <r.felana.gyal@gmail.com>
2017-03-04 20:18:02 +0100

    Add files via upload

--- procedurePers.sql
+++ procedurePers.sql
@@ -1,18 +1,17 @@
 /*PERSONNE*/
 
 DELIMITER |
+ DROP PROCEDURE IF EXISTS ADD_PERSONNE;
+ create procedure ADD_PERSONNE
+ (NomCandidat  Varchar(25),MailCandidat Varchar(25), PrenomPersonne Varchar(25))
 
-	DROP PROCEDURE IF EXISTS ADD_PERSONNE;
-	create procedure ADD_PERSONNE
-	(NomCandidat  Varchar(25),MailCandidat Varchar(25), PrenomPersonne Varchar(25))
-
-	BEGIN
-		DECLARE Num int;
-		insert into CANDIDAT(NumCandidat, NomCandidat) values (null, NomCandidat) ;
-		SELECT MAX(NumCandidat) INTO Num FROM CANDIDAT ; 
-		insert into PERSONNE(PrenomPersonne, NumCandidatPers, MailPers)  values (PrenomPersonne, Num, MailCandidat);
-		
-	END;
+ BEGIN
+  DECLARE Num int;
+  insert into CANDIDAT(NumCandidat, NomCandidat,Equipe) values (null, NomCandidat,0) ;
+  SELECT MAX(NumCandidat) INTO Num FROM CANDIDAT; 
+  insert into PERSONNE(PrenomPersonne, NumCandidatPers, MailPers)  values (PrenomPersonne, Num, MailPers);
+  
+ END;
 |
 
 DELIMITER |
@@ -23,8 +22,8 @@ DELIMITER |
 		SELECT PERSONNE.NumCandidat,NomCandidat,PrenomPersonne,MailCandidat FROM PERSONNE,CANDIDAT WHERE PERSONNE.NumCandidat = CANDIDAT.NumCandidat  ;
 
 	END;
-|
 
+|
 DELIMITER |
 	DROP PROCEDURE IF EXISTS GET_PRENOM_PERSONNE;
 	create procedure GET_PRENOM_PERSONNE (NumCand int(25)) 
@@ -40,14 +39,12 @@ DELIMITER |
 	DROP PROCEDURE IF EXISTS SET_PRENOM_PERSONNE;
 	create procedure SET_PRENOM_PERSONNE (newFirstName varchar(25), NumCand int(25)) 
 	BEGIN
-
 		UPDATE PERSONNE 
 		SET PrenomPersonne = newFirstName
 		WHERE NumCandidat = NumCand;
 
 	END;	
 |
-
 DELIMITER |
 	DROP PROCEDURE IF EXISTS GET_MAIL;
 	create procedure GET_MAIL (NumCand int(25)) 
@@ -58,7 +55,6 @@ DELIMITER |
 
 	END;	
 |
-
 DELIMITER |
 	DROP PROCEDURE IF EXISTS SET_MAIL_PERSONNE;
 	create procedure SET_MAIL_PERSONNE (newFirstMail varchar(25), NumCand int(25)) 
